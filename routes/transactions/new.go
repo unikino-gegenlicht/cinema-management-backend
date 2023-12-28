4f2d859ccb11f4e95960776583e699a6a38f7a85
Jan Eike Suchard <jan@suchard.cloud>
2023-12-28 15:01:38 +0100

    wip: adding counting measures for the tickets
    started implementing the measures for counting the tickets

--- routes/transactions/new.go
+++ routes/transactions/new.go
@@ -12,6 +12,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/bson"
 
+	"github.com/unikino-gegenlicht/cinema-management-backend/database"
 	"github.com/unikino-gegenlicht/cinema-management-backend/middleware"
 	"github.com/unikino-gegenlicht/cinema-management-backend/types"
 )
@@ -30,6 +31,36 @@ func newTransaction(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	// to allow tracking the numver of issued tickets now get the items from the
+	// database
+	itemCollection := database.Database.Collection("items")
+	results, err := itemCollection.Find(r.Context(), bson.D{})
+	if err != nil {
+		log.Error().Err(err).Msg("unable to retrieve items")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var items []types.Item
+	err = results.All(r.Context(), &items)
+	if err != nil {
+		log.Error().Err(err).Msg("unable to retrieve items")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var ticketCount uint
+	for _, itemId := range transaction.Items {
+		for _, item := range items {
+			if item.ID == itemId {
+				ticketCount += item.TicketCount
+			}
+		}
+	}
+
+	log.Debug().Uint("tickets", ticketCount).Msg("issuing new tickets")
+	// todo: issue tickets
 
 	// now insert the transaction
 	_, err = collection.ReplaceOne(r.Context(), bson.D{{"_id", transaction.ID}}, transaction)
