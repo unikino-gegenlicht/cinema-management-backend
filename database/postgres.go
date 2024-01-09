d1d01940ae7ad2383de947d961c0636d518c17f8
Jan Eike Suchard <jan@suchard.cloud>
2024-01-09 16:13:03 +0100

    changing mongo to postgres in code

---
+++ database/postgres.go
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024.  Jan Eike Suchard. All rights reserved
+ * SPDX-License-Identifier: MIT
+ */
+
+package database
+
+import "github.com/jackc/pgx/v5"
+
+var Postgres *pgx.Conn
