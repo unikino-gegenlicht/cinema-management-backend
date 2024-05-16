673b6ae804ae5b11bfc1826da9bdbaaeaf09ba6f
Jan Eike Suchard <jan@suchard.cloud>
2024-05-16 15:13:28 +0200

    creating helpers to retrieve commit hashes and commit timestamps

---
+++ helpers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"runtime/debug"
+)
+
+var Commit = func() string {
+	commitHash := "unkown"
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				commitHash = setting.Value
+			}
+		}
+
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.modified" {
+				commitHash += " (modified)"
+			}
+		}
+	}
+
+	return commitHash
+}()
+
+var CommitTime = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.time" {
+				return setting.Value
+			}
+		}
+	}
+
+	return "unkown"
+}()
