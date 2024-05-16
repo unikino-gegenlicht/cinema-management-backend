8ee985e1a75c95b04cd54fe215d913bbc37b4dba
Jan Eike Suchard <jan@suchard.cloud>
2024-05-16 16:57:08 +0200

    adding database migrations and logging configuration to initialization

--- init.go
+++ init.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/unikino-gegenlicht/cinema-management/backend/db"
+	"github.com/unikino-gegenlicht/cinema-management/backend/logging"
 
 	"github.com/pterm/pterm"
 )
@@ -11,9 +17,25 @@ var logger *slog.Logger
 
 func init() {
 	printStartupMessage()
-	setupLogging()
-	logger.Info("initializing backend")
-	logger.Debug("connecting to database")
+	logging.Configure()
+	logger = logging.Logger
+	logger.Info("starting backend application")
+	err := db.Connect(logger)
+	if err != nil {
+		logger.Error("unable to connect to database", "error", err)
+		os.Exit(1)
+	}
+	logger.Info("connected to the database")
+	logger.Info("running database migrations")
+	err = db.Migrate(logger)
+	if err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			logger.Info("no migrations required")
+		} else {
+			logger.Error("unable to run database migrations", "error", err)
+			os.Exit(1)
+		}
+	}
 }
 
 func printStartupMessage() {
@@ -23,9 +45,4 @@ func printStartupMessage() {
 	msg += fmt.Sprintf("Commit Date: %s\n", CommitTime)
 	msg += "\n© Unikino GEGENLICHT (gegenlicht.net) — Licensed under the MIT License"
 	p.WithCenterEachLineSeparately().Println(msg)
-}
-
-func setupLogging() {
-	handler := pterm.NewSlogHandler(&loggerConfig)
-	logger = slog.New(handler)
 }
